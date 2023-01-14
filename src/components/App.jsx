09245052c8fcaf12ccfe5a8a1d7425fbc3667a72
Daria Halai <106152100+dariahalai@users.noreply.github.com>
2023-01-14 16:17:25 +0300

    refactoring phonebook on hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,61 @@
-export const App = () => {
+import { useState, useEffect } from 'react';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { nanoid } from 'nanoid';
+import { Section, Title } from './App.styled';
+
+import Form from './Form';
+import ContactsList from './ContactsList';
+import Filter from './Filter';
+
+
+export function App() {
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(window.localStorage.getItem('contacts')) ?? [];
+  });
+  const [filter, setFilter] = useState('');
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addNewContact = data => {
+    const newContact = {
+      id: nanoid(),
+      ...data,
+    };
+    contacts.some(({ name }) => name === data.name)
+      ? Notify.warning(`${data.name} is already in contacts`)
+      : setContacts(prev => [...prev, newContact]);
+  };
+
+  const deleteContact = id => {
+    setContacts(prev => prev.filter(contact => contact.id !== id));
+  };
+
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
+  };
+
+  const getVisibleContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Section>
+        <Title>Phonebook</Title>
+        <Form addNewContact={addNewContact} />
+      </Section>
+      <Section>
+        <Title>Contacts</Title>
+        <Filter value={filter} onChange={changeFilter} />
+        <ContactsList
+          contacts={getVisibleContacts()}
+          deleteContact={deleteContact}
+        />
+      </Section>
+    </>
   );
-};
+}
